6f7c5f0a81d7f1856b8e98b8f0f26261e9f6bd49
senumodz-hash <senumodz@gmail.com>
2026-01-13 17:18:14 +0530

    Create contact.js

---
+++ pages/contact.js
@@ -0,0 +1,9 @@
+export default function Contact() {
+  return (
+    <div className="container">
+      <h2>Contact Us</h2>
+      <p>WhatsApp: <a href="https://wa.me/94743903048" className="contact-btn" target="_blank">Chat Now</a></p>
+      <p>Email: <a href="mailto:senumodz@gmail.com" className="contact-btn">senumodz@gmail.com</a></p>
+    </div>
+  )
+}
