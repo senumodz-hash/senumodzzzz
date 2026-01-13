16937009addcfa734ce558a6bbba551f29ce4ca2
senumodz-hash <senumodz@gmail.com>
2026-01-13 17:34:38 +0530

    Update index.js

--- pages/index.js
+++ pages/index.js
@@ -1,11 +1,13 @@
 import Hero from '../components/Hero'
 import Features from '../components/Features'
+import Testimonials from '../components/Testimonials'
 
 export default function Home() {
   return (
     <>
       <Hero />
       <Features />
+      <Testimonials />
     </>
   )
 }
