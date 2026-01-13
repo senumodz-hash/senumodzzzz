5618e42dc26ce34325ddf0b917bb35a887d3539e
senumodz-hash <senumodz@gmail.com>
2026-01-13 17:32:23 +0530

    Update _app.js

--- pages/_app.js
+++ pages/_app.js
@@ -1,7 +1,14 @@
 import '../styles/globals.css'
+import { useEffect } from 'react'
+import AOS from 'aos'
+import 'aos/dist/aos.css'
 import Header from '../components/Header'
 
 function MyApp({ Component, pageProps }) {
+  useEffect(() => {
+    AOS.init({ duration: 800, once: true })
+  }, [])
+
   return (
     <>
       <Header />
