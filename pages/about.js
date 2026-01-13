ce3e3281ec4d2d9f0ecb71b53d45b518d9286378
senumodz-hash <senumodz@gmail.com>
2026-01-13 17:18:34 +0530

    Create about.js

---
+++ pages/about.js
@@ -0,0 +1,8 @@
+export default function About() {
+  return (
+    <div className="container">
+      <h2>About Senu Modz</h2>
+      <p>Senu Modz is your trusted source for tech products. We provide high-quality gadgets and panels with excellent customer service. Our goal is to make tech shopping easy, fast, and reliable!</p>
+    </div>
+  )
+}
