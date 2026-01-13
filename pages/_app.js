239f2a34a888f4d2de5f44a590a9c911b8a2c6c9
senumodz-hash <senumodz@gmail.com>
2026-01-13 17:16:13 +0530

    Update _app.js

--- pages/_app.js
+++ pages/_app.js
@@ -1,21 +1,7 @@
 import '../styles/globals.css'
-import Link from 'next/link'
 
 function MyApp({ Component, pageProps }) {
-  return (
-    <>
-      <header>
-        <h1>Senu Modz</h1>
-        <nav>
-          <Link href="/">Home</Link>
-          <Link href="/products">Products</Link>
-          <Link href="/about">About</Link>
-          <Link href="/contact">Contact</Link>
-        </nav>
-      </header>
-      <Component {...pageProps} />
-    </>
-  )
+  return <Component {...pageProps} />
 }
 
 export default MyApp
