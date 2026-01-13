fed7c1378e9129625275e7b1550aaaabf9414166
senumodz-hash <senumodz@gmail.com>
2026-01-13 17:51:09 +0530

    Update _app.js

--- pages/_app.js
+++ pages/_app.js
@@ -3,16 +3,16 @@ import { useEffect } from 'react'
 import AOS from 'aos'
 import 'aos/dist/aos.css'
 import Header from '../components/Header'
+import AutoMusic from '../components/AutoMusic'
 
 function MyApp({ Component, pageProps }) {
-  useEffect(() => {
-    AOS.init({ duration: 800, once: true })
-  }, [])
+  useEffect(() => { AOS.init({ duration: 800, once: true }) }, [])
 
   return (
     <>
       <Header />
       <Component {...pageProps} />
+      <AutoMusic />
     </>
   )
 }
